5f6e1a02063cfb223ebb6d8bd1682189831022c2
Jean Luiz Zanatta <jl.zanatta92@yahoo.com.br>
2022-05-19 13:25:14 -0300

    Habilitado endpoints actuator.

--- greeting-service/src/main/java/com/zanatta/greetingservice/controllers/GreetingController.java
+++ greeting-service/src/main/java/com/zanatta/greetingservice/controllers/GreetingController.java
@@ -6,7 +6,6 @@ import com.zanatta.greetingservice.configuration.GreetingConfiguration;
 import com.zanatta.greetingservice.models.Greeting;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
     
-    private static final String TEMPLATE = "%s, s%";
+    private static final String TEMPLATE = "%S, %S";
     private final AtomicLong counter = new AtomicLong();
 
     @Autowired GreetingConfiguration greetingConfiguraration;
