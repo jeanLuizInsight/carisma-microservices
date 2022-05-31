7e568b8a4c872dd59c303c606cf37f77d99cb6df
Jean Luiz Zanatta <jean.zanatta@unoesc.edu.br>
2022-05-31 14:36:13 -0300

    Configuração de roteamento dos serviços no api gateway.

---
+++ api-gateway/src/main/java/com/zanatta/apigateway/config/ApiGatewayConfiguration.java
@@ -0,0 +1,29 @@
+package com.zanatta.apigateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfiguration {
+    
+    /**
+     * Configurando as rotas
+     * @param builder
+     * @return
+    */
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+        return builder.routes()
+            // rota para o cambio service
+            .route(fn -> fn.path("/cambio-service/**")
+                // direciona para o load balancer do eureka
+                .uri("lb://cambio-service"))
+            // rota para o book service
+            .route(fn -> fn.path("/book-service/**")
+                // direciona para o load balancer do eureka
+                .uri("lb://book-service"))
+            .build();
+    }
+}
