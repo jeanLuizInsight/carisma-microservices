3f4a251068f27197b8dbf934bba559c647fc055f
jeanLuizInsight <jl.zanatta92@yahoo.com.br>
2022-07-14 20:20:48 -0300

    Adequações para acesso a base de dados.

--- api-gateway/src/main/java/com/zanatta/apigateway/config/ApiGatewayConfiguration.java
+++ api-gateway/src/main/java/com/zanatta/apigateway/config/ApiGatewayConfiguration.java
@@ -18,11 +18,12 @@ public class ApiGatewayConfiguration {
         return builder.routes()
             // rota para o cambio service
             .route(fn -> fn.path("/cambio-service/**")
-                // direciona para o load balancer do eureka
+                // direciona para o load balancer do eureka (naming-server)
                 .uri("lb://cambio-service"))
             // rota para o book service
             .route(fn -> fn.path("/book-service/**")
-                // direciona para o load balancer do eureka
+                // direciona para o load balancer do eureka (naming-server)
+                // dessa forma API Gateway conversa com o Service registry pra balancear
                 .uri("lb://book-service"))
             .build();
     }
--- greeting-service/src/main/java/com/zanatta/greetingservice/configuration/GreetingConfiguration.java
+++ greeting-service/src/main/java/com/zanatta/greetingservice/configuration/GreetingConfiguration.java
@@ -1,9 +1,17 @@
 package com.zanatta.greetingservice.configuration;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
+/**
+ * No arquivo do repositório mapeado em carisma-config-server deve estar com o nome
+ * Novas propriedades devem serem adicionadas aqui e também no arquivo
+ *
+ * @RefreshScope vai pegar as configurações sem precisar parar o serviço. Mas tem que usar o /actuator/refresh
+ */
 @Component
+@RefreshScope
 @ConfigurationProperties("greeting-service")
 public class GreetingConfiguration {
 
